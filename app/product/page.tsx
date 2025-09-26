2bc0f750f7ef96aa4700212ac8e5ac43887c9a72
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-09-26 19:59:51 +0200

    add: Sorter to product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,4 +1,5 @@
 import Filter from "@/components/productPage/Filter";
+import Sorter from "@/components/productPage/Sorter";
 import { getData } from "@/services/getData";
 
 export default async function Product() {
@@ -8,6 +9,9 @@ export default async function Product() {
     <div className="flex px-[40px]">
       <div className="w-1/5">
         <Filter data={dataCat.categories} />
+      </div>
+      <div className="w-4/5">
+        <Sorter />
       </div>
     </div>
   );
---
+++ components/productPage/Sorter.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useState } from "react";
+
+const Sorter = () => {
+  const [sorting, setSorting] = useState<string>("latest");
+  const [showing, setShowing] = useState<string>("3");
+  const showAtOnce = [3, 6, 9, 12];
+  const sortOptions = [
+    { val: "latest", text: "newly listed" },
+    { val: "asc", text: "price highest" },
+    { val: "desc", text: "price lowest" },
+  ];
+
+  return (
+    <div className="flex gap-[60px] pl-[40px]">
+      <div className="flex items-center">
+        <p className="text-[20px] font-semibold text-neutral-50 pr-[16px]">
+          Sort by
+        </p>
+        <select
+          onChange={(e) => setSorting(e.target.value)}
+          className="border rounded-md border-gray-400 bg-neutral-900 px-[16px] py-[10px] text-[14px] text-neutral-50"
+        >
+          {sortOptions.map((opt, index) => (
+            <option key={index} value={opt.val}>
+              {opt.text}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex items-center">
+        <p className="text-[20px] font-semibold text-neutral-50 pr-[16px]">
+          Show
+        </p>
+        <select
+          onChange={(e) => setShowing(e.target.value)}
+          className="border rounded-md border-gray-400 bg-neutral-900 px-[16px] py-[10px] text-[14px] text-neutral-50"
+        >
+          {showAtOnce.map((val, index) => (
+            <option key={index} value={val}>
+              {val}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Sorter;
