9b8454db1b6da19999cee8652185584a6fdec6b8
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-09-30 01:21:08 +0200

    fix

--- services/getData.ts
+++ services/getData.ts
@@ -1,7 +1,7 @@
 export async function getData(url: string) {
   const baseUrl = process.env.DB_HOST;
 
-  const respond = await fetch(`${url}`);
+  const respond = await fetch(`${baseUrl}${url}`);
   const data = await respond.json();
 
   return data;
